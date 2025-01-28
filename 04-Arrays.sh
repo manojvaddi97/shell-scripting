70873b0b30b6b3fc658c3acb0a297102e42fbb13
test-user-cc <manojvaddi497@gmail.com>
2025-01-28 12:56:32 -0500

    added array script

--- 04-Arrays.sh
+++ 04-Arrays.sh
@@ -2,8 +2,8 @@
 
 MOVIES=("Pushpa","RRR","Devara")
 
-echo "First Movie is $MOVIES{[0]}"
-echo "second movie is $MOVIES{[1]}"
-echo "Third movie is $MOVIES{[2]}"
+echo "First Movie is ${MOVIES[0]}"
+echo "second movie is ${MOVIES[1]}"
+echo "Third movie is ${MOVIES[2]}"
 
-echo "ALL movies are $MOVIES[@]"
+echo "ALL movies are ${MOVIES[@]}"
