49ccf8ed3980790a11d7938b8e4a36c104f7d170
test-user-cc <manojvaddi497@gmail.com>
2025-02-03 13:45:17 -0500

    added logs mysql script

--- 10-logs.sh
+++ 10-logs.sh
@@ -1,52 +1,35 @@
 #!/bin/bash
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-DIR="/var/log/shell_scripts"
-LOGFILE=$(echo $0 | cut -d '.' -f1)
-TIMESTAMP=$(date +"%m-%d-%Y-%H-%M-%S")
-LOGFILE_NAME=$DIR/$LOGFILE-$TIMESTAMP
 USERID=$(id -u)
-
-
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 $R Failed.... $N"
-        exit 1
-    # systemctl status mysql # checking status 
-    # if [ $? -ne 0 ]
-    # then
-    #     exit 1
-    else
-        echo -e " $2 $G completed successfully $N"
-    fi
-}
-
-if [ USERID -ne 0 ]
+if [ $USERID -ne 0 ]
 then
-    echo "Error: user need to have sudo privileges"
+    echo "ERROR: you need sudo privileges to perform this action"
     exit 1
 fi
 
-dnf list installed mysql  >>&LOGFILE_NAME # checking if mysql is previously installed or not?
-if [ $? -ne 0 ] # checking if previous command output is successful or not?
-then
-    dnf install mysql -y >>&LOGFILE_NAME # installing mysql
-    VALIDATE $1 "MYSQL Installation"
-else
-    echo -e "MYSQL is $Y already installed $N"
-fi
-
-################################### Installing GIT ############################################
-
-dnf list installed git >>&LOGFILE_NAME # checking if git is previously installed
+dnf list installed mysql
 if [ $? -ne 0 ]
 then
-    dnf install git -y >>&LOGFILE_NAME # installing git
-    VALIDATE $1 "GIT Installation"
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "MYSQL Installation Failed"
+        exit 1
+    else
+        echo "MYSQL Installation Successfull"
 else
-    echo -e "Git is $Y already installed $N"
-fi
+    echo "MYSQL Installation already exits"
+
+
+dnf list installed git
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo "GIT Installation Failed"
+        exit 1
+    else
+        echo "GIT Installation Successfull"
+else
+    echo "GIT Installation already exists"
