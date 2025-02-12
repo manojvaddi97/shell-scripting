63e1119d7425f861bc786f3d31ae8266c9b7e37e
test-user-cc <manojvaddi497@gmail.com>
2025-02-12 15:14:25 -0500

    added count words

--- count_words.sh
+++ count_words.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 FILE_NAME="/home/ec2-user/shell-scripting/sample.txt"
-while IFS=$' \t\n' read -r word;
+while  read -r word;
 do
     echo "word: $word"
-done < "$FILE_NAME"
+done < <(tr -s '[:space:]' '\n' < "$FILE_NAME")
