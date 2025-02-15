0cadf71cea73432ee37dbfa263a9560d7065b86c
test-user-cc <manojvaddi497@gmail.com>
2025-02-14 21:46:07 -0500

    added disk usage

--- disk_usage.sh
+++ disk_usage.sh
@@ -2,10 +2,17 @@
 
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=5
+MSG=""
 
 while read -r usage;
 do
     USAGE=$(echo $usage | awk -F " " '{print $6F}' | cut -d "%" -f1)
     PARTITION=$(echo $usage | awk -F " " '{print $NF}')
     echo "partition: $PARTITION usage is $USAGE"
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        MSG="High Disk usage on Partition: $PARTITION Usage is: $USAGE"
+    fi
 done <<< $DISK_USAGE
+
+echo "Message is $MSG"
