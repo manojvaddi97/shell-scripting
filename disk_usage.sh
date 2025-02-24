7f4591416c64c5a0a8dbbfb44633442a7809274c
test-user-cc <manojvaddi497@gmail.com>
2025-02-24 02:10:36 -0500

    added disk usage

--- disk_usage.sh
+++ disk_usage.sh
@@ -9,10 +9,10 @@ do
     #echo $line
     USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk -F " " '{print $NF}')
-    echo "Partition: $PARTITION , Usage: $USAGE"
+    #echo "Partition: $PARTITION , Usage: $USAGE"
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
-        MSG+="High Disk Usage on partition: $PARTITION Usage is $USAGE"
+        MSG+="High Disk Usage on partition: $PARTITION Usage is $USAGE \n"
     fi
 done <<< $DISK_USAGE
 echo "Message is: $MSG"
