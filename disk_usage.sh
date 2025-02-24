8db0cbe855223ebf89f97a9c52d00c854b28c097
test-user-cc <manojvaddi497@gmail.com>
2025-02-24 02:03:33 -0500

    added disk usage

--- disk_usage.sh
+++ disk_usage.sh
@@ -6,7 +6,7 @@ DISK_THRESHOLD=5
 while read -r line
 do
     #echo $line
-    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -F1)
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk -F " " '{print $NF}')
     echo "Partition: $PARTITION , Usage: $USAGE"
 done <<< $DISK_USAGE
