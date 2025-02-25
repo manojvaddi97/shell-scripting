af9c15a2917bfdeaded333ea9bfd62a539e43ae8
test-user-cc <manojvaddi497@gmail.com>
2025-02-25 09:37:33 -0500

    added file_backup

--- file_backup.sh
+++ file_backup.sh
@@ -2,15 +2,51 @@
 
 #source; destination; daterange; copy; delete
 
-SOURCE="/home/ec2-user/app_logs"
-DESTINATION="/home/ec2-user/app_logs_bkp"
+SOURCE=$1 #pass source folder
+DESTINATION=$2 # pass destination folder
+DAYS=14
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOGFILE_DIR="/home/ec2-user/scripts"
 LOGFILE=$(echo $0 | awk -F "/" '{print $NF}' | cut -d "." -f1 )
 LOGFILE_NAME="$LOGFILE_DIR/$LOGFILE-$TIMESTAMP.log"
 
-mkdir -p $DESTINATION/$TIMESTAMP
-FILES= find $SOURCE -name "*.log" -mtime +14
-cp -r $FILES $DESTINATION/$TIMESTAMP
-gzip $DESTINATION/$TIMESTAMP
-echo "Backup completed on $TIMESTAMP"
+if [ $# -lt 2 ] # comparing if no of arguments passed are less than 2
+then
+    echo "Usage: <script_name> <source_dir> <dest_dir>"
+    exit 1
+fi
+
+if [ ! -d $SOURCE ] #checking if source directory doesn't exits
+then
+    echo "Error: please check if directory exists"
+    exit 1
+fi
+
+if [ ! -d $DESTINATION ] # checking if destination directory doesn't exits
+then
+    echo "Error: please check if directory exists"
+    exit 1
+fi
+
+Files= $(find $SOURCE_DIR -name "*.log" -mtime +$DAYS) # find the files older than 14 days
+
+if [ -n "$FILES" ] # checking if files are present
+then
+    echo "Files are: $FILES"
+    ZIP_FILE="$DESTINATION/app-logs-$TIMESTAMP.zip" # creating zipfile indestination folder
+    find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip @ $ZIP_FILE #ziping the files
+    if [ -f "$ZIP_FILE" ] # if zip files exists
+    then
+        echo "Successfully created zip files"
+        while read -r filepath; # read the files
+        do
+            echo "Deleting files: $filepath"
+            rm -rf $filepath # delete the files
+            echo "Deleted files: $filepath"
+        done <<< $FILES
+    else
+        echo "could not zip the files"
+    fi
+else
+    echo "No files foumd older than $DAYS"
+fi
